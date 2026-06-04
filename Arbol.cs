3c9261c1ff2143aa79c011a866f883c514a2e4c9
EmmmanuelRCPP <164968372+EmmmanuelRCPP@users.noreply.github.com>
2026-06-04 01:23:52 -0700

    dsf

--- Arbol.cs
+++ Arbol.cs
@@ -7,9 +7,25 @@ namespace MyWindowApp
 {
     public partial class Arbol : UserControl
     {
+        private List<string> tokens;
+        private TreeView treeView = new TreeView();
         public void setDisplay(String text)
         {
             display.Text = text;
+            tokens =  SepararTokens(text);
+
+
+            treeView.Nodes.Clear();
+            ParserArbol parser = new ParserArbol();
+            parser.SetParser(tokens);
+            List<string> niveles = parser.Parse();
+            TreeNode root = new TreeNode("Expresion Matematica");
+            foreach (string nivel in niveles)
+            {
+                root.Nodes.Add(nivel);
+            }
+            treeView.Nodes.Add(root);
+            treeView.ExpandAll();
         }
         private List<String> SepararTokens(string expr)
         {
@@ -44,8 +60,13 @@ namespace MyWindowApp
             display.TextAlign = HorizontalAlignment.Right;
             display.Dock = DockStyle.Top;
             display.Height = 80;
+            display.ReadOnly = true;
+
+
 
             Controls.Add(display);
+            treeView.Dock = DockStyle.Fill;
+            panel.Controls.Add(treeView);
 
 
 
